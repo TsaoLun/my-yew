2ce58aba4671e7c62e945134149502a131dd3f34
TsaoLun <ycl18@live.com>
2023-01-04 01:15:02 +0800

    new file:   .gitignore 	new file:   Cargo.lock 	new file:   Cargo.toml 	new file:   dist/index.html 	new file:   index.html 	new file:   src/lib.rs 	new file:   src/main.rs

---
+++ src/lib.rs
@@ -0,0 +1,50 @@
+use gloo::console::log;
+use serde::{Deserialize, Serialize};
+use yew::prelude::*;
+
+#[derive(Serialize, Deserialize)]
+struct MyObject {
+    username: String,
+    favorite_language: String,
+}
+
+#[function_component(App)]
+pub fn app() -> Html {
+    let name = "TsaoLun";
+    let name_obj = MyObject {
+        username: name.to_owned(),
+        favorite_language: "Rust".to_owned(),
+    };
+    log!("Hello! My name is", name);
+    log!(serde_json::to_string_pretty(&name_obj).unwrap());
+    let class = "my_title";
+    // let message = Some("I am a message");
+    let message: Option<&str> = None;
+
+    let tasks = vec!["record video", "grocery shopping", "pet Xilbe"];
+
+    html! {
+        <>
+            <h1 class={class}>{"Hello World!!!"}</h1>
+            if class == "my_titles" {
+                <p>{"Hi there."}</p>
+            } else {
+                <p>{"I'm not a title."}</p>
+            }
+
+            if let Some(message) = message {
+                <p>{message}</p>
+            } else {
+                <p>{"no messages here."}</p>
+            }
+
+            <ul>
+                {list_to_html(tasks)}
+            </ul>
+        </>
+    }
+}
+
+fn list_to_html(list: Vec<&str>) -> Vec<Html> {
+    list.iter().map(|task| html! {<li>{task}</li>}).collect()
+}
---
+++ src/main.rs
@@ -0,0 +1,5 @@
+use my_yew::App;
+
+fn main() {
+    yew::Renderer::<App>::new().render();
+}
